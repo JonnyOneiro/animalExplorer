8b1869cf065fd76338be274843330c90d4f4d99f
Jonathan Gallo <jonnygallo@gmail.com>
2018-09-07 14:52:27 -0700

    pulled new changes by dan

--- webserver.js
+++ webserver.js
@@ -1,13 +1,20 @@
-var animals = require('./data/animals.js');
+
 //require the animals.js file from the data folder, store it in a variable
-
+const animals = require('./data/animals.js');
 //require express
-
+const express = require('express');
 //make the server
-
+const server = express();
 //add the static folder and point it at the html folder
-
+server.use(express.static(__dirname + '/html'));
 //make an endpoint to get data for all animals
 	//return the animals data
-
+server.get('/data/animals', (request, response)=>{
+	console.log('got a request');
+	response.get(JSON.stringify(animals));
+	return animals;
+});
 //make the server listen for connections on port 3000
+server.listen(3000, ()=>{
+    console.log('server is listening on port 3000');
+});
